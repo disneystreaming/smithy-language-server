a5e879323b79cfddf0bc7e86679295d07ca8a39a
omelois <olivier.melois@disneystreaming.com>
2021-11-20 11:50:47 +0100

    Timeouts

--- ci/publish.sh
+++ ci/publish.sh
@@ -12,4 +12,6 @@ mill lsp.publish \
       --sonatypeCreds $SONATYPE_USERNAME:$SONATYPE_PASSWORD \
       --signed true \
       --release true \
+      --readTimeout 600000 \
+      --awaitTimeout 600000 \
       --gpgArgs --passphrase=$PGP_PASSPHRASE,--no-tty,--pinentry-mode,loopback,--batch,--yes,-a,-b
