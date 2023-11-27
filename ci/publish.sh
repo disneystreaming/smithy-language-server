fe266ae3456537ca4e3ac0dd84497bd7436622b5
David Francoeur <dfrancoeur04@gmail.com>
2023-11-27 10:06:37 -0500

    Add quotes in bash script, print gpg version

--- ci/publish.sh
+++ ci/publish.sh
@@ -2,19 +2,21 @@
 
 set -eu
 
-echo $PGP_SECRET | base64 --decode > gpg_key
+gpg --version
+
+echo "$PGP_SECRET" | base64 --decode > gpg_key
 
 gpg --import  --no-tty --batch --yes gpg_key
 
 rm gpg_key
 
 mill lsp.publish \
-      --sonatypeCreds $SONATYPE_USERNAME:$SONATYPE_PASSWORD \
+      --sonatypeCreds "$SONATYPE_USERNAME:$SONATYPE_PASSWORD" \
       --signed true \
       --release true \
       --readTimeout 600000 \
       --awaitTimeout 600000 \
-      --gpgArgs --passphrase=$PGP_PASSPHRASE \
+      --gpgArgs --passphrase="$PGP_PASSPHRASE" \
       --gpgArgs --no-tty \
       --gpgArgs --pinentry-mode \
       --gpgArgs loopback \
